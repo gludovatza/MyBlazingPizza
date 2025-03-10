0dd00384399f03158f942e0210fa2e1efbff2369
Gludovátz Attila <attila@gludovatz.hu>
2025-03-10 11:56:05 +0100

    4. fejezet

--- BlazingPizza.Client/Program.cs
+++ BlazingPizza.Client/Program.cs
@@ -10,4 +10,6 @@ builder.Services.AddScoped(sp => new HttpClient {
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) 
 });
 
+builder.Services.AddScoped<OrderState>();
+
 await builder.Build().RunAsync();
